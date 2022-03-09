44e03ae751bbb83d89b4cc717b2ac9049fb1f6a9
KevinWeber <kevin.ninjabot@gmail.com>
2022-03-09 14:05:15 -0500

    Working on Routing from Landing page to user input page.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './styles/sass/styles.scss';
 import './App.css';
 import riseLogo from './styles/assets/spLogo.png';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
+  const [submit, setSubmit] = useState(false);
+  
   return (
     <div className="App">
         <div className='wrapper'>
@@ -16,6 +21,9 @@ function App() {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
         <button className="button">REGISTER YOUR ACCOUNT</button>
+        <Routes>
+          <Route path="/" element={<UserInputs propSubmit={submit => setSubmit(submit)} />} />
+        </Routes>
       </div>
     </div>
   );
