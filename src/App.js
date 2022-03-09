0a56a370332b90290691f0f24dbfeeb0be4abf02
KevinWeber <kevin.ninjabot@gmail.com>
2022-03-09 14:47:59 -0500

    Happy with current landing page layout

--- src/App.js
+++ src/App.js
@@ -2,31 +2,33 @@ import './styles/sass/styles.scss';
 import './App.css';
 import riseLogo from './styles/assets/spLogo.png';
 import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Link, Routes, Route, Outlet, useParams } from 'react-router-dom';
+import UserInputs from './components/UserInputs.js';
 
 
 function App() {
   const [submit, setSubmit] = useState(false);
   
   return (
-    <div className="App">
-        <div className='wrapper'>
-          <h1>RISE</h1>
-          <h5>by:</h5>
-          <div className='logoBox'>
-            <img src = {riseLogo} alt='The Rise Logo'/>
-          </div>
-        
-          <div className='introParagraph'>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-        <button className="button">REGISTER YOUR ACCOUNT</button>
-        <Routes>
-          <Route path="/" element={<UserInputs propSubmit={submit => setSubmit(submit)} />} />
-        </Routes>
+      <div className="App">
+          <div className='wrapper'>
+            <h1>RISE</h1>
+            <h5>by:</h5>
+            <div className='logoBox'>
+              <img src = {riseLogo} alt='The Rise Logo'/>
+            </div>
+          
+            <div className='introParagraph'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          <Link to="/UserInputs" className='linkToUserInputPage'>REGISTER YOUR ACCOUNT</Link>
+          <Routes>
+            <Route path="/UserInputs"/>
+          </Routes>
+        </div>
       </div>
-    </div>
-  );
+    
+    );
 }
 
 export default App;
--- src/components/UserInputs.js
+++ src/components/UserInputs.js
@@ -1,84 +1,16 @@
-// Modules
-import { useState } from 'react'
-import { useNavigate } from "react-router-dom";
-// Assets
-import arrowPic from "../styles/assets/arrowPicEdited.png";
-// UserInputs.js
-const UserInputs = ({ propTerm, propLimit, propSubmit }) => {
-    const [userLimitChoice] = useState(10);
-    const [userChoice, setUserChoice] = useState("placeholder");
-    const [jokeNumberOne, setJokeNumberOne] = useState("");
-    const [jokeNumberTwo, setJokeNumberTwo] = useState("");
-    const [jokeNumberThree, setJokeNumberThree] = useState("");
-    const navigate = useNavigate();
-    // when the form submits, get the values of each of the user's inputs and set them into useStates.
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const jokeData = [{ joke: jokeNumberOne }, { joke: jokeNumberTwo }, { joke: jokeNumberThree }];
-        const filteredJokeData = jokeData.filter((jokeFilter) => {
-            return jokeFilter.joke !== ""
-        })
-        propTerm(userChoice);
-        propLimit(userLimitChoice - filteredJokeData.length);
-        propSubmit(true);
-        // using navigate here by the recommendation of Adrian and Joey to route the page to /results while pushing the data from the UserInputs component to the Results component.
-        navigate('/results',
-            {
-                state: filteredJokeData
-            }
-        )
-    }
-    return (
-        <div className='wrapper'>
-            <div>
-                <form className="form"
-                    onSubmit={(event) => handleSubmit(event)}
-                >
-                    <label htmlFor="select">Select Your Theme</label>
-                    <select className="dropDown"
-                        id="category"
-                        name="category"
-                        onChange={(e) => setUserChoice(e.target.value)}
-                        value={userChoice}
-                    >
-                        <option value="placeholder" disabled>Choose Your Category</option>
-                        <option value="dogs">Dogs</option>
-                        <option value="cats">Cats</option>
-                        <option value="babies">Babies</option>
-                        <option value="baby animals">Baby Animals</option>
-                        <option value="bunnies">Bunnies</option>
-                    </select>
-                    <div className='customDadDiv'>
-                        <div className="stackBox">
-                            <label htmlFor="jokeNumberOne">Please Add A Dad Joke</label>
-                            <input
-                                type="text"
-                                value={jokeNumberOne}
-                                placeholder="Dad Joke Required"
-                                onChange={e => setJokeNumberOne(e.target.value)}
-                                required
-                            />
-                            <input
-                                type="text"
-                                value={jokeNumberTwo}
-                                placeholder="Enter your dad joke"
-                                onChange={e => setJokeNumberTwo(e.target.value)}
-                            />
-                            <input
-                                type="text"
-                                value={jokeNumberThree}
-                                placeholder="Enter your dad joke"
-                                onChange={e => setJokeNumberThree(e.target.value)}
-                            />
-                        </div>
-                    </div>
-                    <div className='arrowDiv'>
-                        <img src={arrowPic} alt="Click to proceed!" className='arrowImage'></img>
-                        <button type='submit' className='enterButton'>Enter</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
-}
-export default UserInputs;
+// // Modules
+// import { useState } from 'react'
+// import { useNavigate } from "react-router-dom";
+// import { Routes, Route } from 'react-router-dom';
+
+// const UserInputs = ({ propTerm, propLimit, propSubmit }) => {
+    
+
+// return (
+//     <div>
+//         <h2>HAAAAAAAY YOU GUYS</h2>
+//     </div>
+// )
+// }
+
+// export default UserInputs;
