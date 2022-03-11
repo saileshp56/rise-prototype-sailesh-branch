b64c71458fda4edfb1fcd2fe43bf76ec5bd2de54
Joseph Vendryes <79129654+vjoseph3@users.noreply.github.com>
2022-03-11 10:01:56 -0500

    fixed routing

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ function App() {
   return (
       <div className="App">
           <div className='wrapper'>
+          <Routes>
+            <Route path='/' element={<>
             <h1>RISE</h1>
             <h5>by:</h5>
             <div className='logoBox'>
@@ -28,7 +30,7 @@ function App() {
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
           <Link to="/UserInputs" className='linkToUserInputPage'>REGISTER YOUR ACCOUNT</Link>
-          <Routes>
+            </>} />
             <Route path='/UserInputs' element={<UserInputs />} />
           </Routes>
         </div>
